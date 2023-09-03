58994dd7d350dc9a1e97dae7285a6826dc5ea004
Anulax <anulax1225@icloud.com>
2023-09-03 12:05:46 +0200

    Added textboxs and time

--- JeuRaylib/Interface.cs
+++ JeuRaylib/Interface.cs
@@ -5,26 +5,23 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Raylib_cs;
+using static Raylib_cs.Raylib;
 
 namespace GUIInterfaceRaylib
 {
-    public class Button
+    public delegate void Callback();
+    public delegate void Validation(string input);
+    public delegate bool Verifier(string input);
+    public abstract class Container
     {
-        public string name;
+        public string name = "";
+        public string error = "";
         public Vector2 position;
         public Vector2 size;
-        private Rectangle border;
+        protected Rectangle border;
         public Color color;
 
-        public Button(string name, Vector2 position, Vector2 size, Color color)
-        {
-            this.name = name;
-            this.position = position;
-            this.size = size;
-            this.color = color;
-            this.border = Generate();
-        }
-        private Rectangle Generate()
+        protected Rectangle Generate()
         {
             return new Rectangle((int)this.position.X, (int)this.position.Y, (int)this.size.X, (int)this.size.Y);
         }
@@ -45,5 +42,109 @@ namespace GUIInterfaceRaylib
         {
             return this.border;
         }
+
+        public bool CheckCollison(Vector2 ptn)
+        {
+            return CheckCollisionPointRec(ptn, this.border);
+        }
     }
+    public class TextBox : Container
+    {
+        private Verifier verifier;
+        private Validation cbOnValidation;
+        private bool canWrite = false;
+        public bool isWriting = false;
+        public List<char> inputsBuffer = new List<char>();
+
+        public TextBox(string name, Vector2 position, Vector2 size, Color color, bool canWrite, Verifier verifie, Validation cbOnValidation)
+        {
+            this.name = name;
+            this.position = position;
+            this.size = size;
+            this.color = color;
+            this.canWrite = canWrite;
+            this.verifier = verifie;
+            this.cbOnValidation = cbOnValidation;
+            this.border = Generate();
+        }
+
+        public void EnabledWriting()
+        {
+            this.canWrite = true;
+        }
+
+        public bool CanWrite()
+        {
+            return this.canWrite;
+        }
+
+        public void SetStrVerifier(Verifier verifier)
+        {
+            this.verifier = verifier;
+        }
+
+        public string GetBufferInput()
+        {
+            return String.Concat(this.inputsBuffer);
+        }
+
+        public bool GetVerification()
+        {
+            return this.verifier(GetBufferInput());
+        }
+
+        public void OnValidation()
+        {
+            this.cbOnValidation(this.GetBufferInput());
+        }
+
+        public void FlushBuffer()
+        {
+            this.inputsBuffer.Clear();
+        }
+
+        public static char Write() 
+        {
+            int keyPressed = GetCharPressed();
+            Dictionary<KeyboardKey, char> tbKeyBoard = new Dictionary<KeyboardKey, char> {
+                { KeyboardKey.KEY_ONE , '1'}, { KeyboardKey.KEY_TWO , '2' }, { KeyboardKey.KEY_THREE , '3'}, { KeyboardKey.KEY_FOUR , '4'}, { KeyboardKey.KEY_FIVE, '5' }, { KeyboardKey.KEY_SIX , '6'},
+                { KeyboardKey.KEY_SEVEN, '7' }, { KeyboardKey.KEY_EIGHT, '8' }, { KeyboardKey.KEY_NINE, '9' }, { KeyboardKey.KEY_ZERO, '0' }, { KeyboardKey.KEY_A, 'a' }, { KeyboardKey.KEY_B, 'b' },
+                { KeyboardKey.KEY_C, 'c' }, { KeyboardKey.KEY_D, 'd' }, { KeyboardKey.KEY_E, 'e' }, { KeyboardKey.KEY_F, 'f' }, { KeyboardKey.KEY_G, 'g' }, { KeyboardKey.KEY_H, 'h' }, { KeyboardKey.KEY_I, 'i' },
+                { KeyboardKey.KEY_J, 'j' }, { KeyboardKey.KEY_K, 'k' }, { KeyboardKey.KEY_L, 'l' }, { KeyboardKey.KEY_M, 'm' }, { KeyboardKey.KEY_N, 'n' }, { KeyboardKey.KEY_O, 'o' }, { KeyboardKey.KEY_P, 'p' },
+                { KeyboardKey.KEY_Q , 'q'}, { KeyboardKey.KEY_R, 'r' }, { KeyboardKey.KEY_S, 's' }, { KeyboardKey.KEY_T, 't' }, { KeyboardKey.KEY_U, 'u' }, { KeyboardKey.KEY_V, 'v' }, { KeyboardKey.KEY_W, 'w' },
+                { KeyboardKey.KEY_X, 'x' }, { KeyboardKey.KEY_Y, 'y' }, { KeyboardKey.KEY_Z, 'z' }, { KeyboardKey.KEY_ENTER, '\0' }
+            };
+            foreach (var key  in tbKeyBoard)
+            {
+                if (IsKeyPressed(key.Key)) return key.Value;
+            }
+            return '$';
+        }
+    }
+
+    public class Button : Container
+    {
+        private Callback cbOnClick;
+        public Button(string name, Vector2 position, Vector2 size, Color color, Callback cb)
+        {
+            this.name = name;
+            this.position = position;
+            this.size = size;
+            this.color = color;
+            this.cbOnClick = cb;
+            this.border = Generate();
+        }
+
+        public void SetCallBack(Callback cbOnClick)
+        {
+            this.cbOnClick = cbOnClick;
+        }
+        public void OnClick()
+        {
+            if (cbOnClick != null)
+            {
+                this.cbOnClick();
+            }
+        }
+    } 
 }
--- JeuRaylib/Newton.cs
+++ JeuRaylib/Newton.cs
@@ -18,22 +18,27 @@ namespace Newton
 
         private bool isCamFixed = true;
         private bool isInfoHidden = false;
+        bool protectedContainer = false;
+
         private float zoom = 1f;
         private float timeStep = 1f;
+
         private int indexClick = -1;
         private int camTarget = -1;
 
         private Random rnd = new Random();
-        private Button btnNewBody;
+        
         private List<Button> lstBtns = new List<Button>();
+        private List<TextBox> lstTextBox = new List<TextBox>();
         private List<MassiveBody> lstBodys = new List<MassiveBody>();
+
         private Camera2D cam = new Camera2D();
+        private Vector2 camMouv = new Vector2(0, 0);
 
         public SpatialManager2DRender(uint screenWidth, uint screenHeight)
         {
             this.screenWidth = (int)(screenWidth);
             this.screenHeight = (int)(screenHeight);
-            this.btnNewBody = GenerateButton("New body", 1200, -1000, 300, 100, Color.BROWN);
         }
 
         private void Init()
@@ -44,8 +49,38 @@ namespace Newton
             InitWindow(this.screenWidth, this.screenHeight, "Simu de Newton");
             // Set our game to run at 60 frames-per-second
             SetTargetFPS(60);
-            // Button
-            lstBtns.Add(btnNewBody);
+            this.InitInterface();
+        }
+
+        private void InitInterface()
+        {
+            lstBtns.Add(GenerateButton("New body", 1200, -1000, 300, 100, Color.BROWN, () =>
+            {
+                lstBodys.Add(GenerateBody(MassiveBody.CreatName(), 1, 1, rnd.Next(100) + 50, rnd.Next(100) + 10, rnd.Next(9) - rnd.Next(9), rnd.Next(9) - rnd.Next(9), rndColor()));
+            }));
+
+            lstTextBox.Add(GenerateTextBox("Change radius", 1200, -850, 300, 100, Color.BROWN, (string input) =>
+            {
+                try { return Convert.ToInt32(input) > 0 && Convert.ToInt32(input) < 1000 ? true : false; }
+                catch { return false; }
+            },
+            (string input) =>
+            {
+                if (this.indexClick >= 0) this.lstBodys[this.indexClick].SetRadius(Convert.ToInt32(input));
+            }));
+
+            lstTextBox.Add(GenerateTextBox("Change name", 1200, -700, 300, 100, Color.BROWN, (string input) =>
+            {
+                if (!string.IsNullOrEmpty(input))
+                {
+                    return true;
+                }
+                return false;
+            },
+            (string input) =>
+            {
+                if (this.indexClick >= 0) this.lstBodys[this.indexClick].name = input;
+            }));
         }
 
         public void Start()
@@ -63,6 +98,7 @@ namespace Newton
                 ClearBackground(Color.BLACK);
 
                 this.MouvBody();
+                this.DrawTextBox();
                 this.DrawButtons();
                 this.DrawBodys();
                 this.DrawConditional();
@@ -85,29 +121,105 @@ namespace Newton
             this.indexClick = -1;
             this.camTarget = -1;
             this.lstBtns = new List<Button>();
+            this.lstTextBox = new List<TextBox>();
             this.lstBodys = new List<MassiveBody>();
+            this.camMouv = new Vector2(0, 0);
             this.cam = new Camera2D();
             this.Init();
         }
 
         private void GetUserInput()
         {
-            float mouseWheelScroll = GetMouseWheelMove();
 
             Vector2 vMouse = GetMousePosition();
             vMouse *= 2;
             vMouse.X -= this.screenWidth;
             vMouse.Y -= this.screenHeight;
 
-            if (mouseWheelScroll > 0 && this.zoom < 20)
+            foreach (Button btn in lstBtns)
             {
-                this.zoom += 0.5f;
+                if (btn.CheckCollison(vMouse) && IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT))
+                {
+                    btn.OnClick();
+                }
             }
-            if (mouseWheelScroll < 0 && this.zoom > 0.5)
+            if (indexClick >= 0) this.GetTextBoxInput(vMouse);
+            if (!this.protectedContainer) this.GetKeyCommandInput(vMouse);
+        }
+        private void GetTextBoxInput(Vector2 vMouse)
+        {
+            foreach (TextBox textBox in lstTextBox)
             {
-                this.zoom -= 0.5f;
+                if ((textBox.CheckCollison(vMouse) && IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)) || textBox.isWriting)
+                {
+                    DisableCursor();
+                    textBox.isWriting = true;
+                    protectedContainer = true;
+                    char input = TextBox.Write();
+                    if (input == '\0')
+                    {
+                        if (textBox.GetVerification())
+                        {
+                            textBox.OnValidation();
+                            textBox.error = "";
+                        }
+                        else
+                        {
+                            textBox.error = "Typing error";
+                        }
+                        protectedContainer = false;
+                        textBox.isWriting = false;
+                        textBox.FlushBuffer();
+                        EnableCursor();
+
+                    }
+                    else if (!(input == '$'))
+                    {
+                        textBox.inputsBuffer.Add(input);
+                    }
+                }
+            }
+        }
+
+        private void GetKeyCommandInput(Vector2 vMouse)
+        {
+            float mouseWheelScroll = GetMouseWheelMove();
+
+            // Zoom Input
+            if (mouseWheelScroll > 0 && this.zoom < 100)
+            {
+                this.zoom *= 2;
+            }
+            if (mouseWheelScroll < 0)
+            {
+                this.zoom *= 0.5f;
             }
 
+            // Camera mouvement Input
+            if (IsKeyDown(KeyboardKey.KEY_W))
+            {
+                this.camMouv.Y += 4;
+            }
+            else if (IsKeyDown(KeyboardKey.KEY_S))
+            {
+                this.camMouv.Y -= 4;
+            }
+            else if (IsKeyDown(KeyboardKey.KEY_A))
+            {
+                this.camMouv.X += 4;
+            }
+            else if (IsKeyDown(KeyboardKey.KEY_D))
+            {
+                this.camMouv.X -= 4;
+            }
+            else
+            {
+                this.camMouv.X = 0;
+                this.camMouv.Y = 0;
+            }
+            MouvBodys(camMouv);
+
+            //Command Input
             if (IsKeyPressed(KeyboardKey.KEY_SPACE))
             {
                 this.timeStep = 0;
@@ -118,27 +230,22 @@ namespace Newton
                 this.isInfoHidden = !this.isInfoHidden;
             }
 
-            if (IsKeyPressed(KeyboardKey.KEY_A))
+            if (IsKeyPressed(KeyboardKey.KEY_R))
             {
                 this.camTarget = this.indexClick;
                 this.isCamFixed = !this.isCamFixed;
             }
-
             if (IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT))
             {
                 this.indexClick = FindOnTarget(vMouse);
             }
 
-            if (CheckCollisionPointRec(vMouse, btnNewBody.GetBorder()) && IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT))
-            {
-                Console.WriteLine("OUI");
-                lstBodys.Add(GenerateBody(MassiveBody.CreatName(), 1, 1, rnd.Next(150), rnd.Next(50), rnd.Next(4) - rnd.Next(4), rnd.Next(4) - rnd.Next(4), rndColor()));
-            }
-            if (IsKeyPressed(KeyboardKey.KEY_B))
+            if (IsKeyDown(KeyboardKey.KEY_UP))
             {
                 timeStep += 0.1f;
             }
-            if (IsKeyPressed(KeyboardKey.KEY_C))
+
+            if (IsKeyDown(KeyboardKey.KEY_DOWN))
             {
                 timeStep -= 0.1f;
             }
@@ -171,12 +278,20 @@ namespace Newton
             return body;
         }
 
-        private static Button GenerateButton(string name, int x, int y, int width, int height, Color color)
+        private static Button GenerateButton(string name, int x, int y, int width, int height, Color color, Callback cb)
         {
             Vector2 position = new Vector2(x, y);
             Vector2 size = new Vector2(width, height);
-            Button button = new Button(name, position, size, color);
+            Button button = new Button(name, position, size, color, cb);
             return button;
+        }
+
+        private static TextBox GenerateTextBox(string name, int x, int y, int width, int height, Color color, Verifier verifier, Validation cb)
+        {
+            Vector2 position = new Vector2(x, y);
+            Vector2 size = new Vector2(width, height);
+            TextBox textBox = new TextBox(name, position, size, color, true, verifier, cb);
+            return textBox;
         }
 
         private void MouvBody()
@@ -198,10 +313,15 @@ namespace Newton
             {
                 Vector2 deltaDestination = WorldToScreen(this.lstBodys[this.camTarget].position) - this.cam.target;
                 deltaDestination *= -1;
-                foreach (MassiveBody body in this.lstBodys)
-                {
-                    body.position += deltaDestination;
-                }
+                MouvBodys(deltaDestination);
+            }
+        }
+
+        private void MouvBodys(Vector2 deltaDestination)
+        {
+            foreach (MassiveBody body in this.lstBodys)
+            {
+                body.position += deltaDestination;
             }
         }
 
@@ -219,10 +339,6 @@ namespace Newton
             }
             return -1;
         }
-        private void ModifieInfo(MassiveBody body)
-        {
-
-        }
 
         private void DrawButtons()
         {
@@ -234,6 +350,33 @@ namespace Newton
                 DrawRectangle((int)screenPos.X, (int)screenPos.Y, (int)btn.size.X, (int)btn.size.Y, btn.color);
                 int textLenght = MeasureText(btn.name, fontSize);
                 DrawText(btn.name, (int)(screenPos.X + (btn.size.X - textLenght) / 2), (int)(textOffsetY + screenPos.Y + btn.size.Y / 2), fontSize, Color.WHITE);
+            }
+        }
+
+        private void DrawTextBox()
+        {
+            int textOffsetY = -20;
+            int fontSize = 40;
+            foreach (TextBox textBox in this.lstTextBox)
+            {
+                Vector2 screenPos = WorldToScreen(textBox.position);
+                DrawRectangle((int)screenPos.X, (int)screenPos.Y, (int)textBox.size.X, (int)textBox.size.Y, textBox.color);
+                string input = textBox.GetBufferInput();
+                if (textBox.isWriting && !(input == null))
+                {
+                    int textLenght = MeasureText(input, fontSize);
+                    DrawText(input, (int)(screenPos.X + (textBox.size.X - textLenght) / 2), (int)(textOffsetY + screenPos.Y + textBox.size.Y / 2), fontSize, Color.WHITE);
+                } 
+                else if (!String.IsNullOrEmpty(textBox.error))
+                {
+                    int textLenght = MeasureText(textBox.error, fontSize);
+                    DrawText(textBox.error, (int)(screenPos.X + (textBox.size.X - textLenght) / 2), (int)(textOffsetY + screenPos.Y + textBox.size.Y / 2), fontSize, Color.WHITE);
+                } 
+                else
+                {
+                    int textLenght = MeasureText(textBox.name, fontSize);
+                    DrawText(textBox.name, (int)(screenPos.X + (textBox.size.X - textLenght) / 2), (int)(textOffsetY + screenPos.Y + textBox.size.Y / 2), fontSize, Color.WHITE);
+                }
             }
         }
 
@@ -263,6 +406,7 @@ namespace Newton
                 }
             }
         }
+
         private void DrawParamInfo(MassiveBody body)
         {
             float textOffset = 40f;
@@ -272,11 +416,12 @@ namespace Newton
             DrawText(String.Format("Name : {0}", body.name), textPosX, (int)(pos.Y + body.radius + textOffset), fontSize, Color.WHITE);
             DrawText(String.Format("Speed : {0}", VectorTools.Vector2Normalize(body.speed), 2), textPosX, (int)(pos.Y + body.radius + textOffset * 2), fontSize, Color.WHITE);
             DrawText(String.Format("Masse : {0}", body.masse), textPosX, (int)(pos.Y + body.radius + textOffset * 3), fontSize, Color.WHITE);
+            DrawText(String.Format("Radius : {0}", body.radius), textPosX, (int)(pos.Y + body.radius + textOffset * 4), fontSize, Color.WHITE);
         }
 
         private void DrawCommand()
         {
-            int fontSize = 40;
+            int fontSize = 35;
             int textOffsetY = 40;
             int textPosX = (int)(this.cam.target.X - this.cam.offset.X / cam.zoom);
             int textPosY = (int)(this.cam.target.Y - this.cam.offset.Y / cam.zoom);
@@ -284,13 +429,15 @@ namespace Newton
             DrawText("Press V to see the bodys speed Vector.", textPosX, textPosY + textOffsetY, fontSize, Color.WHITE);
             DrawText("Left click on a body to get additional info.", textPosX, textPosY + textOffsetY * 2, fontSize, Color.WHITE);
             DrawText("Press M to hide/show the menu.", textPosX, textPosY + textOffsetY * 3, fontSize, Color.WHITE);
-            DrawText("Press A to be centered after left clicking.", textPosX, textPosY + textOffsetY * 4, fontSize, Color.WHITE);
+            DrawText("Press R to be centered after left clicking.", textPosX, textPosY + textOffsetY * 4, fontSize, Color.WHITE);
+            DrawText("Press UP to Fast forward time. ", textPosX, textPosY + textOffsetY * 5, fontSize, Color.WHITE);
+            DrawText("Press DOWN to go back in time.", textPosX, textPosY + textOffsetY * 6, fontSize, Color.WHITE);
         }
     }
 
     public class MassiveBody
     {
-        const float CONSTGRAVITATION = 0.1f;
+        const float CONSTGRAVITATION = 1f;
 
         public string name;
         public Vector2 position;
@@ -305,11 +452,17 @@ namespace Newton
             this.name = name;
             this.position = position;
             this.speed = vitesse;
-            this.radius = radius;
             this.surfaceG = surfaceG;
             this.color = color;
+            this.SetRadius(radius);
+        }
+
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
             this.masse = (surfaceG * radius * radius) / CONSTGRAVITATION;
         }
+
         public void Graviter(List<MassiveBody> lstBody, float timeStep)
         {
             foreach (MassiveBody body in lstBody)
